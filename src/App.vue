7efe80ea63697408f2f354a38c2d088ab8a6d776
Shih-Po Wang <shihpo@gmail.com>
2020-03-05 15:27:49 +0900

    init vuejs proj

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <span>Test</span>
+    <router-view></router-view>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+
+export default {
+
+  data: () => ({
+    taskData: 'Hello'
+  }),
+
+}
+</script>
